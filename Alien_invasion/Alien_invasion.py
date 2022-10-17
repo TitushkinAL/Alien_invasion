2d576961c45fe74533251274730f5a595f1ac93a
Александр Титушкин <titushkinaleksandr@rambler.ru>
2022-10-17 21:40:31 +0300

    refactoring game_function

--- Alien_invasion/Alien_invasion.py
+++ Alien_invasion/Alien_invasion.py
@@ -14,11 +14,13 @@ def run_game():
 
     #creating ship
     ship = Ship(ai_settings, screen)
+    bullets = Group()
 
     while True:
 
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets.update()
         gf.update_screen(ai_settings, screen, ship)
         
 run_game()
--- Alien_invasion/game_functions.py
+++ Alien_invasion/game_functions.py
@@ -1,6 +1,21 @@
 import sys
-
 import pygame
+from bullet import Bullet
+
+
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 
 def check_events(ship):
     #processing event_clicks
@@ -8,19 +23,11 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    ship.moving_left = True
-
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-
-
+            check_keyup_events(event, ship)
+        
+                
 def update_screen(ai_settings, screen, ship):
     screen.fill(ai_settings.bg_color)
     ship.blitme()
